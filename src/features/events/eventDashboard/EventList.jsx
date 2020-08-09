990b9e6b4be259db9b9fa134161b80a0b31b4222
siegblink <siegblink@gmail.com>
2020-08-09 23:20:23 +0800

    Add 'EventList' component.

---
+++ src/features/events/eventDashboard/EventList.jsx
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import EventListItem from './EventListItem'
+
+export default function EventList() {
+  return (
+    <Fragment>
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+      <EventListItem />
+    </Fragment>
+  )
+}
